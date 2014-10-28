513fe03d4b9367c4993eadb23038088b1697c181
Christopher Konopka <cskonopka@gmail.com>
2014-10-28 00:42:50 -0400

    code/pdmodel fixes

--- PureData/model3_PD_feedback/arduivis_PD_model3_feedback/arduivis_PD_model3_feedback.ino
+++ PureData/model3_PD_feedback/arduivis_PD_model3_feedback/arduivis_PD_model3_feedback.ino
@@ -3,7 +3,7 @@
 ~~~~~~~ arduivis - Pure Data ~~~~~~~
 ~~~~~ model#3: feedback ~~~~~
 
-pd patch: arduivis_model3_PD_feedback.pd
+PD: arduivis_model3_PD_feedback.pd
 
 This is an example of to read sliders from PD. 
 An Arduino reads and parses this data, sending
@@ -19,43 +19,43 @@ This example code is in the public domain.
 
 void setup() 
 {
-// Create/open serial port
-  Serial.begin(9600);	 	
+  // Create/open serial port
+    Serial.begin(9600);   
 
   // Define LED mode 
   // PWM LED
-  pinMode(3, OUTPUT);
+    pinMode(3, OUTPUT);
 }
 
 void loop() 
 {
-	// Slider from Pure Data
-	int pdSlider;
-	
-	// Feedback from Pure Data (original output from Arduino)
-	int feedbackFromPD;
-	
-	// Parse incoming Pure Data slider values
-	// from Pure Data, to Arduino	
-	pdSlider = Serial.parseInt();		
-	 
-	// Write parsed slider values to serial buffer
-	// [comport] object
-	// to Pure Data, from Arduino
-	Serial.write(pdSlider);		
-	
-	// Parse incoming Pure Data (feedback) values
-	// from Pure Data (feedback), to Arduino 
-	feedbackFromPD = Serial.parseInt();
-	
-	// Write parsed values to LEDs
-	// Fading LED
-	// from Pure Data (feedback), to Arduino
-	analogWrite(3, feedbackFromPD);	
-	
-	// Write feedback values to serial buffer
-	// [comport] object
-	// to Pure Data, from Arduino
-	// This is useful for debugging
-	Serial.write(feedbackFromPD);
+  // Slider from Pure Data
+    int pdSlider;
+  
+  // Feedback from Pure Data (original output from Arduino)
+    int feedbackFromPD;
+  
+  // Parse incoming Pure Data slider values
+  // from Pure Data, to Arduino 
+    pdSlider = Serial.parseInt();   
+   
+  // Write parsed slider values to serial buffer
+  // [comport] object
+  // to Pure Data, from Arduino
+    Serial.write(pdSlider);   
+  
+  // Parse incoming Pure Data (feedback) values
+  // from Pure Data (feedback), to Arduino 
+    feedbackFromPD = Serial.parseInt();
+  
+  // Write parsed values to LEDs
+  // Fading LED
+  // from Pure Data (feedback), to Arduino
+    analogWrite(3, feedbackFromPD); 
+  
+  // Write feedback values to serial buffer
+  // [comport] object
+  // to Pure Data, from Arduino
+  // This is useful for debugging
+    Serial.write(feedbackFromPD);
 }
