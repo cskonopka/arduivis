b3bc5737d71ec762e1f8dfc24a404d5c3e05948e
Christopher Konopka <cskonopka@gmail.com>
2015-01-05 22:37:33 -0500

    cleaning up Bluetooth/Python examples

--- Bluetooth/Using the HC-06 Bluetooth Module   MCU on Eclipse_files/rate.php
+++
@@ -1,6 +0,0 @@
-PDRTJS_5506978_post_8335.avg_rating = 5;
-PDRTJS_5506978_post_8335.votes = 13;		
-PDRTJS_settings_5506978_post_8335= {"type" : "stars","size" : "sml","star_color" : "yellow","custom_star" : "","font_size" : "","font_line_height" : "16px","font_color" : "","font_align" : "left","font_position" : "right","font_family" : "","font_bold" : "normal","font_italic" : "normal","text_vote" : "Vote","text_votes" : "Votes","text_rate_this" : "Rate This","text_1_star" : "Very Poor","text_2_star" : "Poor","text_3_star" : "Average","text_4_star" : "Good","text_5_star" : "Excellent","text_thank_you" : "Thank You","text_rate_up" : "Rate Up","text_rate_down" : "Rate Down"};
-PDRTJS_5506978_post_8335.init();		
-PDRTJS_5506978_post_8335.token='7c9943f7877c50e62c5ee1b1035de069';
-/*5506978,_post_8335,wp-post-8335,406606598,5-13*/
