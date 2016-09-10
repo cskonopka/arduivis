02e188b2f1500cdd4a38f9452196f81ed062420d
Christopher Konopka <cskonopka@gmail.com>
2016-09-10 18:22:23 -0400

    stupid js fix

--- arduivisJS/arduivisJS.js
+++ arduivisJS/arduivisJS.js
@@ -247,6 +247,7 @@ function connectObjects(type){
 		_arduivis.connect(vserialport, 0, vsel, 0);
 		_arduivis.connect(vmettog, 0, vmetro, 0);
 		_arduivis.connect(vmetro, 0, vatoi, 0);
+		_arduivis.connect(vmetro, 0, vserialport, 0);	
 		_arduivis.connect(vatoi, 0, vapp10, 0);	
 		_arduivis.connect(vapp10, 0, vserialport, 0);
 		_arduivis.connect(vpak, 0, vatoi, 2);
@@ -270,10 +271,12 @@ function connectObjects(type){
 		_arduivis.connect(vprependport, 0, vserialport, 0);
 		_arduivis.connect(vserialport, 0, vsel, 0);
 		_arduivis.connect(vsel, 0, vzl, 0);
+		_arduivis.connect(vsel, 2, vzl, 0);		
 		_arduivis.connect(vzl, 0, vitoa, 0);
 		_arduivis.connect(vitoa, 0, vsym, 0);
 		_arduivis.connect(vmettog, 0, vmetro, 0);
 		_arduivis.connect(vmetro, 0, vatoi, 0);
+		_arduivis.connect(vmetro, 0, vserialport, 0);			
 		_arduivis.connect(vatoi, 0, vapp10, 0);	
 		_arduivis.connect(vapp10, 0, vserialport, 0);
 		_arduivis.connect(vpak, 0, vatoi, 2);
